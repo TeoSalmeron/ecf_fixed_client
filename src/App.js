8e310e7a1f59f76fa9b41ae934f3ccf87d03a478
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-12 19:59:10 +0200

    set all folders and files from previous project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import React from 'react'
+// Pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import DashboardAdmin from "./pages/DashboardAdmin"
+import DashboardEmployee from "./pages/DashboardEmployee"
+import Logout from "./pages/Logout"
+import Cars from "./pages/Cars"
 
-const App = () => {
+// Components
+import ProtectedRoute from "./components/ProtectedRoute"
+import Nav from "./components/Nav"
+
+// Router
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+
+// React auth kit
+import createStore from 'react-auth-kit/createStore'
+import AuthProvider from 'react-auth-kit'
+
+const store = createStore({
+  authName:'_auth',
+  authType:'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:',
+})
+
+function App() {
   return (
-    <div>App</div>
+    <AuthProvider
+      store={store}
+    >
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/cars" element={<Cars />}/>
+          <Route path="/dashboard/admin" element={
+            <ProtectedRoute>
+              <DashboardAdmin />
+            </ProtectedRoute>
+          }/>
+          <Route path="/dashboard/employee" element={
+            <ProtectedRoute>
+              <DashboardEmployee />
+            </ProtectedRoute>
+          }
+            
+          />
+          <Route path="/logout" element={
+            <ProtectedRoute>
+              <Logout />
+            </ProtectedRoute>
+          }/>
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
-export default App
+export default App;
