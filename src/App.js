a3e96c213a98e1bac4f133e5477ba89164f5dc13
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-12 19:33:28 +0200

    added babel dev dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// Pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import DashboardAdmin from "./pages/DashboardAdmin"
+import DashboardEmployee from "./pages/DashboardEmployee"
+import Logout from "./pages/Logout"
+import Cars from "./pages/Cars"
+
+// Components
+import ProtectedRoute from "./components/ProtectedRoute"
+import Nav from "./components/Nav"
+
+// Router
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+
+// React auth kit
+import createStore from 'react-auth-kit/createStore'
+import AuthProvider from 'react-auth-kit'
+
+const store = createStore({
+  authName:'_auth',
+  authType:'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:',
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthProvider
+      store={store}
+    >
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/cars" element={<Cars />}/>
+          <Route path="/dashboard/admin" element={
+            <ProtectedRoute>
+              <DashboardAdmin />
+            </ProtectedRoute>
+          }/>
+          <Route path="/dashboard/employee" element={
+            <ProtectedRoute>
+              <DashboardEmployee />
+            </ProtectedRoute>
+          }
+            
+          />
+          <Route path="/logout" element={
+            <ProtectedRoute>
+              <Logout />
+            </ProtectedRoute>
+          }/>
+        </Routes>
+      </Router>
+    </AuthProvider>
+  )
 }
 
 export default App;
