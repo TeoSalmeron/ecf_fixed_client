5d381b0e25960ebb590da5a84b55a87ff1deddd0
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-12 22:06:30 +0200

    Fixed logout

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -8,32 +8,32 @@ const Logout = () => {
 
     useEffect(() => {
         // Check if user is already logged
-        // axios.get(process.env.REACT_APP_API_URL + "/auth",
-        // {
-        //     withCredentials: true 
-        // })
-        // .then((response) => {
-        //     if(!response.data.auth) {
-        //         window.location.assign("/login")
-        //     }
-        // })
-        // .catch((error) => {
-        //     console.log(error)
-        // })
+        axios.get(process.env.REACT_APP_API_URL + "/auth",
+        {
+            withCredentials: true 
+        })
+        .then((response) => {
+            if(!response.data.auth) {
+                window.location.assign("/login")
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
         
-        // // Disconnect user
-        // axios.get(process.env.REACT_APP_API_URL + "/logout",
-        // {
-        //     withCredentials: true
-        // })
-        // .then((response) => {
-        //     if(response.data.logout) {
-        //         window.location.assign("/")
-        //     }
-        // })
-        // .catch((error) => {
-        //     console.log(error)
-        // })
+        // Disconnect user
+        axios.get(process.env.REACT_APP_API_URL + "/logout",
+        {
+            withCredentials: true
+        })
+        .then((response) => {
+            if(response.data.logout) {
+                window.location.assign("/")
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }, [])
 
   return (
